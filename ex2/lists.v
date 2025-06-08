298f3b0c2c42ffad35e3f330aa55751effe8d2fe
Koren Ma'avari <koren130@gmail.com>
2025-06-09 01:48:16 +0300

    Q1

--- ex2/lists.v
+++ ex2/lists.v
@@ -1,16 +1,26 @@
 Set Implicit Arguments.
 Require Import Lists.List.
 Import ListNotations.
+Require Import Lia.
 
 Print list.
 Print rev.
 
 
 (* Q1. Define `is_sorted`.  (should return a Prop.)  *)
+Inductive is_sorted {A : Type} (R : A -> A -> Prop) : list A -> Prop :=
+| sorted_nil : is_sorted R []
+| sorted_singleton : forall x, is_sorted R [x]
+| sorted_cons : forall x y l, R x y -> is_sorted R (y :: l) -> is_sorted R (x :: y :: l).
 
 
 (* Show that this list is sorted. *)
-Lemma warm_up : is_sorted [3;5;9] le.
+Lemma warm_up : is_sorted le [3;5;9].
+Proof.
+  apply sorted_cons. lia.
+  apply sorted_cons. lia.
+  apply sorted_singleton.
+Qed.
 
 
 (* Q2. Prove that an ascending list of nat, when reversed, 
