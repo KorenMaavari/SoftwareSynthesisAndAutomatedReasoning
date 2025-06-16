07d386f5cada678ae37e2bd42161c1c5a55823ac
Koren Ma'avari <koren130@gmail.com>
2025-06-16 18:34:10 +0300

    Q2 (the last line does not work yet)

--- ex2/lists.v
+++ ex2/lists.v
@@ -25,7 +25,36 @@ Qed.
 
 (* Q2. Prove that an ascending list of nat, when reversed, 
  *     becomes a descending list. *)
+Lemma sorted_append_one :
+  forall (A : Type) (R : A -> A -> Prop) (l : list A) (a b : A),
+    is_sorted R (l ++ [a]) ->
+    R a b ->
+    is_sorted R (l ++ [a; b]).
+Proof.
+  intros A R l a b Hsorted Rab.
+  induction l as [| x xs IH].
+  - simpl in *. (* [] ++ [a] = [a], so goal becomes is_sorted R [a; b] *)
+    apply sorted_cons. assumption. apply sorted_singleton.
+  - simpl in *. (* l = x :: xs, so l ++ [a] = x :: xs ++ [a] *)
+    destruct xs as [| y ys].
+    + inversion Hsorted as [| | x' y' l' Hxy Hsorted']; subst.
+      apply sorted_cons. assumption.
+      apply sorted_cons. assumption. constructor.
+    + inversion Hsorted as [| | x' y' l' Hxy Hsorted']; subst.
+      apply sorted_cons. assumption.
+      apply IH. assumption.
+Qed.
 
 (*     Don't forget to use the _hint_ when proving it. *)
 
-Theorem rev_asc_desc : ...
+Theorem rev_asc_desc :
+  forall l, is_sorted le l -> is_sorted ge (rev l).
+Proof.
+  intros l H.
+  induction H.
+  - simpl. constructor.
+  - simpl. constructor.
+  - simpl.
+    rewrite <- app_assoc.
+    apply (sorted_append_one nat ge rev l ++ [y; x]); auto. (* Does not work yet *)
+Qed.
